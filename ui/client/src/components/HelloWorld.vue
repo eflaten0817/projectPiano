759499d79d33fb09c4184970989f30f03111250b
Erik <ef936963@wcupa.edu>
2021-12-10 14:25:28 -0500

    not sure if all of these are needed in repo...

--- ui/client/src/components/HelloWorld.vue
+++ ui/client/src/components/HelloWorld.vue
@@ -1,6 +1,29 @@
 <template>
   <v-container>
-    Hello World
+    <v-data-iterator :items="pianoWorks">
+      <template v-slot:default="props">
+          <v-row>
+            <v-col
+              v-for="item in props.items"
+              :key="item._id"
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+            >
+              <v-card>
+                <v-card-title class="subheading font-weight-bold">
+                  {{ item.MovementPieceTitle }}
+                </v-card-title>
+  
+                <v-divider></v-divider>
+  
+                
+              </v-card>
+            </v-col>
+          </v-row>
+        </template>
+    </v-data-iterator>
   </v-container>
 </template>
 
@@ -11,60 +34,12 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      pianoWorks: undefined,
     }),
     async created() {
       console.log('hello world');
-      const works = await getAllworks();
+      let works = await getAllworks();
+      this.pianoWorks = works;
       console.log(works);
       return true;
     },
